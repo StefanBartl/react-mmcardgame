2a93b35b1c1f53ecc699d0915d638345225c1a92
WKDStevie <50949225+StefanBartl@users.noreply.github.com>
2022-05-28 20:52:31 +0200

    Updates

---
+++ precache-manifest.418e6f25d824c74d3d923e0f3a4230ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cca9f91bfefc0b96994",
+    "url": "/react-mmcardgame/static/css/main.4a0aeb97.chunk.css"
+  },
+  {
+    "revision": "7cca9f91bfefc0b96994",
+    "url": "/react-mmcardgame/static/js/main.edf91be2.chunk.js"
+  },
+  {
+    "revision": "9c333fdee93af36ee48a",
+    "url": "/react-mmcardgame/static/js/runtime~main.e0255286.js"
+  },
+  {
+    "revision": "0f7a7b3b0fb9aa42f365",
+    "url": "/react-mmcardgame/static/js/2.0e52f05c.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/react-mmcardgame/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "d7c157e808e3b9687cbcc2e16a800b67",
+    "url": "/react-mmcardgame/index.html"
+  }
+];
